7f5041ac38b4536f0e454ece255afc6b7557d708
RoperLee <pengleemanager@163.com>
2017-06-09 17:35:29 +0800

    end

--- src/main/java/com/computer/boot/model/Chapter.java
+++ src/main/java/com/computer/boot/model/Chapter.java
@@ -3,17 +3,24 @@ package com.computer.boot.model;
 /**
  * Created by roper on 2017/6/9.
  */
-public class Chapter extends BasicModelObject{
+public class Chapter extends BasicModelObject {
 
     private String title;
     private Integer subjectId;
 
-    public String getTitle() { return title; }
+    public String getTitle() {
+        return title;
+    }
 
-    public void setTitle(String title) { this.title = title; }
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-    public Integer getParentSubject() { return subjectId; }
+    public Integer getSubjectId() {
+        return subjectId;
+    }
 
-    public void setParentSubject(Integer subjectId) { this.subjectId = subjectId; }
-
+    public void setSubjectId(Integer subjectId) {
+        this.subjectId = subjectId;
+    }
 }
--- src/main/java/com/computer/boot/model/Directory.java
+++ src/main/java/com/computer/boot/model/Directory.java
@@ -3,26 +3,42 @@ package com.computer.boot.model;
 /**
  * Created by roper on 2017/6/9.
  */
-public class Directory extends BasicModelObject{
+public class Directory extends BasicModelObject {
 
     private String title;
     private Integer chapterId;
     private Integer subjectId;
-    private IssueKind kind;
+    private String kind;
 
-    public Integer getChapterId() { return chapterId; }
+    public Integer getChapterId() {
+        return chapterId;
+    }
 
-    public void setChapterId(Integer chapterId) { this.chapterId = chapterId; }
+    public void setChapterId(Integer chapterId) {
+        this.chapterId = chapterId;
+    }
 
-    public Integer getSubjectId() { return subjectId; }
+    public Integer getSubjectId() {
+        return subjectId;
+    }
 
-    public void setSubjectId(Integer subjectId) { this.subjectId = subjectId; }
+    public void setSubjectId(Integer subjectId) {
+        this.subjectId = subjectId;
+    }
 
-    public String getTitle() { return title; }
+    public String getTitle() {
+        return title;
+    }
 
-    public void setTitle(String title) { this.title = title; }
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-    public IssueKind getKind() { return kind; }
+    public String getKind() {
+        return kind;
+    }
 
-    public void setKind(IssueKind kind) { this.kind = kind; }
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
 }
